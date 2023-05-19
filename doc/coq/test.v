4579726a090fb4b37fc85291677b20e33ad47cb0
jhlee <sunglasses99@snu.ac.kr>
2023-05-19 20:57:55 +0900

    Prove transitivity

--- doc/coq/test.v
+++ doc/coq/test.v
@@ -29,6 +29,9 @@ with expr_tm :=
   | e_link (m : mod_tm) (e : expr_tm)
 .
 
+Scheme expr_ind_mut := Induction for expr_tm Sort Prop
+with mod_ind_mut := Induction for mod_tm Sort Prop.
+
 Inductive ctx :=
   | c_hole
   | c_lam (x: expr_id) (C : ctx)
@@ -66,6 +69,8 @@ Fixpoint eq_p (p1 p2 : path) :=
                     else false
   end.
 
+Definition len_p (p : path) := List.length p.
+
 Definition eq_eid id1 id2 :=
   match id1, id2 with
   | eid x1, eid x2 => x1 =? x2
@@ -74,6 +79,16 @@ Definition eq_eid id1 id2 :=
 Definition eq_mid id1 id2 :=
   match id1, id2 with
   | mid M1, mid M2 => M1 =? M2
+  end.
+
+Fixpoint level (C : ctx) :=
+  match C with
+  | c_hole => 0
+  | c_app_left C' _
+  | c_app_right _ C' => level C'
+  | c_lam _ C'
+  | c_lete _ _ C' 
+  | c_letm _ _ C' => S (level C')
   end.
 
 Fixpoint index_expr_aux (C : ctx) (x : expr_id) (o : option nat) :=
@@ -171,18 +186,17 @@ Inductive EevalR (p : path) (C : ctx) (st : state)
     : EevalR p C st (e_lam x e) (Closure x e p C) st
   | Eeval_app e1 e2 
               x e_lam p_lam C_lam st_lam
-              arg e_mem_a m_mem_a t_a
+              arg e_mem m_mem t
               v st_v
               (FN : EevalR p (C [|c_app_left c_hole e2|]) st
                            e1 (Closure x e_lam p_lam C_lam) st_lam)
               (ARG : EevalR p (C [|c_app_right e1 c_hole|]) st_lam
-                            e2 arg (ST e_mem_a m_mem_a t_a))
-              (BODY : EevalR (t_a :: p_lam) (C_lam [|c_lam x c_hole|])
-                             (ST ((t_a :: p_lam) !-> arg ; e_mem_a) m_mem_a (update_t t_a))
+                            e2 arg (ST e_mem m_mem t))
+              (BODY : EevalR (t :: p_lam) (C_lam [|c_lam x c_hole|])
+                             (ST ((t :: p_lam) !-> arg ; e_mem) m_mem (update_t t))
                               e_lam v st_v)
     : EevalR p C st (e_app e1 e2) v st_v
-  | Eeval_link m e p_m C_m st_m 
-               v st_v
+  | Eeval_link m e p_m C_m st_m v st_v
                (MOD : MevalR p C st m (Mod p_m C_m) st_m)
                (LINK : EevalR p_m C_m st_m e v st_v)
     : EevalR p C st (e_link m e) v st_v
@@ -213,20 +227,15 @@ with MevalR (p : path) (C : ctx) (st : state)
     : MevalR p C st (m_letm M m1 m2) v_m2 st_m2
 .
 
+Scheme EevalR_ind_mut := Induction for EevalR Sort Prop
+with MevalR_ind_mut := Induction for MevalR Sort Prop.
+
 (* Reachability relation *)
 Inductive EreachE (p : path) (C : ctx) (st : state)
     : expr_tm -> path -> ctx -> state -> expr_tm -> Prop :=
   | ere_refl e 
     : EreachE p C st e 
               p C st e
-  | ermre_trans e p_m C_m st_m m
-                p' C' st' e'
-              (REACHm : EreachM p C st e
-                                p_m C_m st_m m)
-              (REACHe' : MreachE p_m C_m st_m m
-                                 p' C' st' e')
-    : EreachE p C st e
-              p' C' st' e'
   | ere_app_left e1 e2 p' C' st' e'
                  (REACHl : EreachE p (C [|c_app_left c_hole e2|]) st e1
                                    p' C' st' e')
@@ -249,53 +258,60 @@ Inductive EreachE (p : path) (C : ctx) (st : state)
                                     p' C' st' e')
     : EreachE p C st (e_app e1 e2)
               p' C' st' e'
-  | ere_link m e p_m C_m st_m p' C' st' e'
+  | ere_link_m m e p' C' st' e'
+               (REACHm : MreachE p C st m
+                                 p' C' st' e')
+    : EreachE p C st (e_link m e)
+              p' C' st' e'
+  | ere_link_e m e p_m C_m st_m p' C' st' e'
                (MOD : MevalR p C st m (Mod p_m C_m) st_m)
                (REACHe : EreachE p_m C_m st_m e
                                  p' C' st' e')
     : EreachE p C st (e_link m e)
               p' C' st' e'
 
-with EreachM (p : path) (C : ctx) (st : state)
-    : expr_tm -> path -> ctx -> state -> mod_tm -> Prop :=
-  | erm_link m e
-    : EreachM p C st (e_link m e)
-              p C st m
-  | ermrm_trans e p_m C_m st_m m p' C' st' m'
-                (REACHm : EreachM p C st e
-                                  p_m C_m st_m m)
-                (REACHm' : MreachM p_m C_m st_m m
-                                   p' C' st' m')
-    : EreachM p C st e
-              p' C' st' m'
-
 with MreachE (p : path) (C : ctx) (st : state)
     : mod_tm -> path -> ctx -> state -> expr_tm -> Prop :=
-  | mre_lete x e m
+  | mre_lete_e x e m p' C' st' e'
+               (REACHe : EreachE p C st e
+                                 p' C' st' e')
     : MreachE p C st (m_lete x e m)
-              p C st e
-  | mrere_trans m p_e C_e st_e e p' C' st' e'
-                (REACHe : MreachE p C st m
-                                  p_e C_e st_e e)
-                (REACHe' : EreachE p_e C_e st_e e
-                                   p' C' st' e')
-    : MreachE p C st m
               p' C' st' e'
+  | mre_lete_m x e m v e_mem m_mem t_e
+             p' C' st' e'
+             (EVALx : EevalR p C st e v (ST e_mem m_mem t_e))
+             (REACHm : MreachE (t_e :: p) (C[|c_lete x e c_hole|]) (ST ((t_e :: p) !-> v ; e_mem) m_mem (update_t t_e)) m
+                                p' C' st' e')
+    : MreachE p C st (m_lete x e m)
+              p' C' st' e'
+  | mre_letm_m1 M m1 m2 p' C' st' e'
+               (REACHm : MreachE p C st m1
+                                 p' C' st' e')
+    : MreachE p C st (m_letm M m1 m2)
+              p' C' st' e'
+  | mre_letm_m2 M m1 m2 v e_mem m_mem t_m
+             p' C' st' e'
+             (EVALM : MevalR p C st m1 v (ST e_mem m_mem t_m))
+             (REACHm : MreachE (t_m :: p) (C[|c_letm M m1 c_hole|]) (ST e_mem ((t_m :: p) !-> v ; m_mem) (update_t t_m)) m2
+                                p' C' st' e' )
+    : MreachE p C st (m_letm M m1 m2)
+              p' C' st' e'
+.
 
-with MreachM (p : path) (C : ctx) (st : state)
+Scheme EreachE_ind_mut := Induction for EreachE Sort Prop
+with MreachE_ind_mut := Induction for MreachE Sort Prop.
+
+Inductive MreachM (p : path) (C : ctx) (st : state)
     : mod_tm -> path -> ctx -> state -> mod_tm -> Prop :=
   | mrm_refl m
     : MreachM p C st m
               p C st m
-  | mrerm_trans m p_e C_e st_e e
-                p' C' st' m'
-              (REACHe : MreachE p C st m
-                                p_e C_e st_e e)
-              (REACHm' : EreachM p_e C_e st_e e
+  | mrm_lete_e x e m p' C' st' m'
+               (REACHe : EreachM p C st e
                                  p' C' st' m')
-    : MreachM p C st m
-              p C' st' m'
-  | mrm_lete x e m v e_mem m_mem t_e
+    : MreachM p C st (m_lete x e m)
+              p' C' st' m'
+  | mrm_lete_m x e m v e_mem m_mem t_e
              p' C' st' m'
              (EVALx : EevalR p C st e v (ST e_mem m_mem t_e))
              (REACHm : MreachM (t_e :: p) (C[|c_lete x e c_hole|]) (ST ((t_e :: p) !-> v ; e_mem) m_mem (update_t t_e)) m
@@ -309,12 +325,51 @@ with MreachM (p : path) (C : ctx) (st : state)
               p' C' st' m'
   | mrm_letm_m2 M m1 m2 v e_mem m_mem t_m
              p' C' st' m'
-             (EVALm1 : MevalR p C st m1 v (ST e_mem m_mem t_m))
+             (EVALM : MevalR p C st m1 v (ST e_mem m_mem t_m))
              (REACHm : MreachM (t_m :: p) (C[|c_letm M m1 c_hole|]) (ST e_mem ((t_m :: p) !-> v ; m_mem) (update_t t_m)) m2
                                 p' C' st' m' )
     : MreachM p C st (m_letm M m1 m2)
               p' C' st' m'
+
+with EreachM (p : path) (C : ctx) (st : state)
+    : expr_tm -> path -> ctx -> state -> mod_tm -> Prop :=
+  | erm_app_left e1 e2 p' C' st' m'
+                 (REACHl : EreachM p (C [|c_app_left c_hole e2|]) st e1
+                                   p' C' st' m')
+    : EreachM p C st (e_app e1 e2) 
+              p' C' st' m'
+  | erm_app_right e1 e2 v1 st1 p' C' st' m'
+                  (FN : EevalR p (C [|c_app_left c_hole e2|]) st e1 v1 st1)
+                  (REACHr : EreachM p (C[|c_app_right e1 c_hole|]) st1 e2
+                                    p' C' st' m')
+    : EreachM p C st (e_app e1 e2)
+              p' C' st' m'
+  | erm_app_body e1 e2 x e_lam p_lam C_lam st_lam
+                 arg e_mem m_mem t_a
+                 p' C' st' m'
+                 (FN : EevalR p (C [|c_app_left c_hole e2|]) st e1 
+                             (Closure x e_lam p_lam C_lam) st_lam)
+                 (ARG : EevalR p (C [|c_app_right e1 c_hole|]) st_lam e2
+                               arg (ST e_mem m_mem t_a))
+                 (REACHb : EreachM (t_a :: p_lam) (C_lam[|c_lam x c_hole|]) (ST ((t_a :: p_lam) !-> arg ; e_mem) m_mem (update_t t_a)) e_lam
+                                    p' C' st' m')
+    : EreachM p C st (e_app e1 e2)
+              p' C' st' m'
+  | erm_link_m m e p' C' st' m'
+               (REACHm : MreachM p C st m
+                                 p' C' st' m')
+    : EreachM p C st (e_link m e)
+              p' C' st' m'
+  | erm_link_e m e p_m C_m st_m p' C' st' m'
+               (MOD : MevalR p C st m (Mod p_m C_m) st_m)
+               (REACHe : EreachM p_m C_m st_m e
+                                 p' C' st' m')
+    : EreachM p C st (e_link m e)
+              p' C' st' m'
 .
+
+Scheme MreachM_ind_mut := Induction for MreachM Sort Prop
+with EreachM_ind_mut := Induction for EreachM Sort Prop.
 
 (* Tactics from Gil Hur, from his Software Foundations Class *)
 
@@ -334,18 +389,134 @@ Ltac revert_until id :=
 Ltac ginduction H :=
   move H at top; revert_until H; induction H.
 
-Lemma ere_trans : forall e1 p1 C1 st1
-                         e2 p2 C2 st2
-                         e3 p3 C3 st3
-                        (REACH1 : EreachE p1 C1 st1 e1
-                                          p2 C2 st2 e2)
-                        (REACH2 : EreachE p2 C2 st2 e2
-                                          p3 C3 st3 e3),
-                        EreachE p1 C1 st1 e1
-                                p3 C3 st3 e3.
+Ltac inv H := inversion H; subst; clear H.
+
+Lemma ere_trans : forall p1 C1 st1 e1
+                         p2 C2 st2 e2
+                         p3 C3 st3 e3
+                         (REACH1 : EreachE p1 C1 st1 e1
+                                           p2 C2 st2 e2)
+                         (REACH2 : EreachE p2 C2 st2 e2
+                                           p3 C3 st3 e3),
+                         EreachE p1 C1 st1 e1
+                                 p3 C3 st3 e3.
 Proof.
-  intros. 
-  ginduction REACH1; eauto using EreachE, EreachM, MreachE, MreachM.
+  intros. generalize dependent e3.
+  revert p3 C3 st3.
+  apply (EreachE_ind_mut
+    (fun p1 C1 st1 e1 
+         p2 C2 st2 e2 
+         (REACH1 : EreachE p1 C1 st1 e1
+                           p2 C2 st2 e2)=> 
+         forall p3 C3 st3 e3 (REACH2 : EreachE p2 C2 st2 e2
+                                               p3 C3 st3 e3),
+                EreachE p1 C1 st1 e1
+                        p3 C3 st3 e3)
+    (fun p1 C1 st1 m1
+         p2 C2 st2 e2
+         (REACH1 : MreachE p1 C1 st1 m1
+                           p2 C2 st2 e2) =>
+         forall p3 C3 st3 e3 (REACH2 : EreachE p2 C2 st2 e2
+                                               p3 C3 st3 e3),
+                MreachE p1 C1 st1 m1
+                        p3 C3 st3 e3));
+   eauto using EreachE, MreachE.
+Qed.
+
+Lemma mrm_trans : forall p1 C1 st1 m1
+                         p2 C2 st2 m2
+                         p3 C3 st3 m3
+                         (REACH1 : MreachM p1 C1 st1 m1
+                                           p2 C2 st2 m2)
+                         (REACH2 : MreachM p2 C2 st2 m2
+                                           p3 C3 st3 m3),
+                         MreachM p1 C1 st1 m1
+                                 p3 C3 st3 m3.
+Proof.
+  intros. generalize dependent m3.
+  revert p3 C3 st3.
+  apply (MreachM_ind_mut
+    (fun p1 C1 st1 m1 
+         p2 C2 st2 m2 
+         (REACH1 : MreachM p1 C1 st1 m1
+                           p2 C2 st2 m2)=> 
+         forall p3 C3 st3 m3 (REACH2 : MreachM p2 C2 st2 m2
+                                               p3 C3 st3 m3),
+                MreachM p1 C1 st1 m1
+                        p3 C3 st3 m3)
+    (fun p1 C1 st1 e1
+         p2 C2 st2 m2
+         (REACH1 : EreachM p1 C1 st1 e1
+                           p2 C2 st2 m2) =>
+         forall p3 C3 st3 m3 (REACH2 : MreachM p2 C2 st2 m2
+                                               p3 C3 st3 m3),
+                EreachM p1 C1 st1 e1
+                        p3 C3 st3 m3));
+   eauto using MreachM, EreachM.
+Qed.
+
+Lemma ermre_trans : forall p1 C1 st1 e1
+                           p2 C2 st2 m2
+                           p3 C3 st3 e3
+                           (REACH1 : EreachM p1 C1 st1 e1
+                                             p2 C2 st2 m2)
+                           (REACH2 : MreachE p2 C2 st2 m2
+                                             p3 C3 st3 e3),
+                           EreachE p1 C1 st1 e1
+                                   p3 C3 st3 e3. 
+Proof.
+  intros. generalize dependent e3.
+  revert p3 C3 st3.
+  apply (EreachM_ind_mut
+    (fun p1 C1 st1 m1
+         p2 C2 st2 m2
+         (REACH1 : MreachM p1 C1 st1 m1
+                           p2 C2 st2 m2)=>
+         forall p3 C3 st3 e3 (REACH2 : MreachE p2 C2 st2 m2
+                                               p3 C3 st3 e3),
+                MreachE p1 C1 st1 m1
+                        p3 C3 st3 e3)
+    (fun p1 C1 st1 e1
+         p2 C2 st2 m2
+         (REACH1 : EreachM p1 C1 st1 e1
+                           p2 C2 st2 m2)=>
+         forall p3 C3 st3 e3 (REACH2 : MreachE p2 C2 st2 m2
+                                               p3 C3 st3 e3),
+                EreachE p1 C1 st1 e1
+                        p3 C3 st3 e3));
+    eauto using EreachE, EreachM, MreachE, MreachM.
+Qed.
+
+Lemma mrerm_trans : forall p1 C1 st1 m1
+                           p2 C2 st2 e2
+                           p3 C3 st3 m3
+                           (REACH1 : MreachE p1 C1 st1 m1
+                                             p2 C2 st2 e2)
+                           (REACH2 : EreachM p2 C2 st2 e2
+                                             p3 C3 st3 m3),
+                           MreachM p1 C1 st1 m1
+                                   p3 C3 st3 m3. 
+Proof.
+  intros. generalize dependent m3.
+  revert p3 C3 st3.
+  apply (MreachE_ind_mut
+    (fun p1 C1 st1 e1
+         p2 C2 st2 e2
+         (REACH1 : EreachE p1 C1 st1 e1
+                           p2 C2 st2 e2)=>
+         forall p3 C3 st3 m3 (REACH2 : EreachM p2 C2 st2 e2
+                                               p3 C3 st3 m3),
+                EreachM p1 C1 st1 e1
+                        p3 C3 st3 m3)
+    (fun p1 C1 st1 m1
+         p2 C2 st2 e2
+         (REACH1 : MreachE p1 C1 st1 m1
+                           p2 C2 st2 e2)=>
+         forall p3 C3 st3 m3 (REACH2 : EreachM p2 C2 st2 e2
+                                               p3 C3 st3 m3),
+                MreachM p1 C1 st1 m1
+                        p3 C3 st3 m3));
+    eauto using EreachE, EreachM, MreachE, MreachM.
 Qed.
 
 Lemma c_plugin_assoc : forall C1 C2 C3, C1[| C2[| C3 |] |] = ((C1[|C2|])[|C3|]).
@@ -353,3 +524,17 @@ Proof.
   induction C1; eauto using ctx; 
   intros; simpl; try rewrite IHC1; eauto.
 Qed.
+
+Lemma c_plugin_adds_level : forall C1 C2, level(C1[|C2|]) = level C1 + level C2.
+Proof.
+  induction C1; eauto using ctx;
+  intros; simpl; try rewrite IHC1; eauto.
+Qed.
+
+(* Want to show this *)
+Lemma ere_len_p_is_level : 
+    forall e st p C st' e'
+           (REACH : EreachE [] c_hole st e
+                            p C st' e'),
+            len_p p = level C.
+Proof. Admitted.
